6a7656fe70d52fa6cdb024089f8df025d931d79e
Kamil <kamil.wojcik@git.com>
2014-11-05 21:00:09 +0100

    Revert "Revert "pierwszy commit""
    This reverts commit 36d0b9bdef97dd070bce1989e5f2c98f546bfcc0.

---
+++ GitProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pierwsza linia");
+
+
+        }
+    }
+}
