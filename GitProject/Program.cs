bc203141efbf51f97f257ec620cbb9c3c53632e6
kamilw <kamil.wojcik@git.com>
2014-11-06 16:10:40 +0100

    dodana linia

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -10,6 +10,8 @@ namespace GitProject
         static void Main(string[] args)
         {
             Console.WriteLine("pierwsza linia");
+            Console.WriteLine("pierwsza linia");
+            Console.WriteLine("pierwsza linia");
 
 
         }
